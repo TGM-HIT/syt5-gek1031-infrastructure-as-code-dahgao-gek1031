929465b1b6ecdc39de2e9b3b3091b4a44c9accfe
Simon Gao <simon.shubin@gmail.com>
2025-03-06 15:04:22 +0100

    added main.tf with 2 webservers and a hcloud loadbalancer

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+
+resource "hcloud_server" "webservers" {
+  count       = 2
+  name        = "webserver-${count.index + 1}"
+  image       = "ubuntu-22.04" # Adjust if needed
+  server_type = "cax11"
+  location    = "nbg1"
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y && sudo apt upgrade -y",
+      "sudo apt install -y nginx",
+      "sudo systemctl enable --now nginx",
+      "echo '<h1>Hello from webserver-${count.index + 1}!</h1>' | sudo tee /var/www/html/index.html"
+    ]
+  }
+}
+
+resource "hcloud_load_balancer" "lb" {
+  name     = "web-lb"
+  load_balancer_type = "lb11"
+  location = "nbg1"
+}
+
+resource "hcloud_load_balancer_target" "web_targets" {
+  count             = 2
+  load_balancer_id  = hcloud_load_balancer.lb.id
+  type              = "server"
+  server_id         = hcloud_server.webservers[count.index].id
+}
+
+resource "hcloud_load_balancer_service" "http" {
+  load_balancer_id = hcloud_load_balancer.lb.id
+  protocol         = "http"
+  listen_port      = 80
+  destination_port = 80
+}
